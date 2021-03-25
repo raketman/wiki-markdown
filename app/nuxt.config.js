874f17e08f7ee3cecf392ccc2f99f913c9bd8eb9
raketman <raketmandevelop@gmail.com>
2021-03-26 01:15:49 +0300

    Подключил bootsrap + sidebae

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -42,7 +42,8 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    'bootstrap-vue/nuxt'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
