f618d4aad50ccb9d5b2930585151ed6499842ccc
raketman <raketmandevelop@gmail.com>
2021-03-26 01:34:58 +0300

    Парсинг markdown

--- app/components/Page.vue
+++ app/components/Page.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
     <div v-if="isLoading">Идет загрузка</div>
-    <div v-if="!isLoading" v-html="content"></div>
+    <div v-if="!isLoading">
+      <vue-markdown  :source="content"></vue-markdown>
+    </div>
   </div>
 </template>
 
 <script>
+import VueMarkdown from '@adapttive/vue-markdown'
+
 export default {
   name: "Page",
+  components: {
+    VueMarkdown
+  },
   computed: {
     isLoading() {
      return this.$store.getters['wiki/page'] === null
